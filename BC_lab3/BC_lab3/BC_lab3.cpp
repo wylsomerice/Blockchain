5d06703ba489c256bd4701c4c77d996e846574f8
Artem Ponomarev <ponomarev@fort-telecom.ru>
2024-03-05 17:53:01 +0500

    Добавил сравнение прямого и параллельного решения

--- BC_lab3/BC_lab3/BC_lab3.cpp
+++ BC_lab3/BC_lab3/BC_lab3.cpp
@@ -3,10 +3,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
-#include <omp.h>
 
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
 const double EPSILON = 1e-10;
 
@@ -20,7 +19,31 @@ void printMatrix(const vector<vector<double>>& matrix) {
     }
 }
 
-vector<double> gaussElimination(vector<vector<double>>& matrix) {
+vector<double> gaussEliminationSequential(vector<vector<double>>& matrix) {
+    int n = matrix.size();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            double ratio = matrix[j][i] / matrix[i][i];
+            for (int k = i; k < n + 1; ++k) {
+                matrix[j][k] -= ratio * matrix[i][k];
+            }
+        }
+    }
+
+    vector<double> solution(n);
+    for (int i = n - 1; i >= 0; --i) {
+        solution[i] = matrix[i][n];
+        for (int j = i + 1; j < n; ++j) {
+            solution[i] -= matrix[i][j] * solution[j];
+        }
+        solution[i] /= matrix[i][i];
+    }
+
+    return solution;
+}
+
+vector<double> gaussEliminationParallel(vector<vector<double>>& matrix) {
     int n = matrix.size();
 
     for (int i = 0; i < n; ++i) {
@@ -49,15 +72,16 @@ vector<double> gaussElimination(vector<vector<double>>& matrix) {
 int main() {
 
     setlocale(LC_ALL, "RUSSIAN");
+    int n;
+    cout << "Введите размерность матрицы: ";
+    cin >> n;
 
-    int n = 10;
-  
     vector<vector<double>> matrix(n, vector<double>(n + 1));
 
     srand(time(0));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n + 1; ++j) {
-            matrix[i][j] = rand() % 9 + 1; 
+            matrix[i][j] = rand() % 10 + 1; 
         }
     }
 
@@ -66,17 +90,27 @@ int main() {
     cout << endl;
 
     auto start = high_resolution_clock::now();
-
-    vector<double> solution = gaussElimination(matrix);
-
+    vector<double> solutionSeq = gaussEliminationSequential(matrix);
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(stop - start);
+    auto durationSeq = duration_cast<microseconds>(stop - start);
 
-    cout << "Время выполнения: " << duration.count() << " миллисекунд" << endl;
+    cout << "Время выполнения (без OpenMP): " << durationSeq.count() << " микросекунд" << endl;
+
+    start = high_resolution_clock::now();
+    vector<double> solutionPar = gaussEliminationParallel(matrix);
+    stop = high_resolution_clock::now();
+    auto durationPar = duration_cast<microseconds>(stop - start);
+
+    cout << "Время выполнения (с OpenMP): " << durationPar.count() << " микросекунд" << endl;
 
     cout << "Решение СЛАУ:" << endl;
+    cout << "Без OpenMP:" << endl;
     for (int i = 0; i < n; ++i) {
-        cout << "x" << i + 1 << " = " << solution[i] << endl;
+        cout << "x" << i + 1 << " = " << solutionSeq[i] << endl;
+    }
+    cout << "С OpenMP:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "x" << i + 1 << " = " << solutionPar[i] << endl;
     }
 
     return 0;
