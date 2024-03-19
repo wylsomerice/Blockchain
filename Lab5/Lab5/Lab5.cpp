70cafc901d1a5ffb51265f5f12d54a1b7eec472f
Artem Ponomarev <ponomarev@fort-telecom.ru>
2024-03-19 18:06:51 +0500

    правки

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -1,21 +1,35 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <chrono>
 #include <mpi.h>
 
 using namespace std;
 using namespace std::chrono;
 
-const int N = 100; // Размерность матрицы
+const int N = 2000; 
 
-// Умножение части матрицы A на матрицу B
-void matrixMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B, vector<vector<double>>& C, int startRow, int endRow) {
-    for (int i = startRow; i < endRow; ++i) {
-        for (int j = 0; j < N; ++j) {
-            C[i][j] = 0;
-            for (int k = 0; k < N; ++k) {
-                C[i][j] += A[i][k] * B[k][j];
+void forwardElimination(vector<vector<double>>& A, vector<double>& b, int startRow, int endRow) {
+    for (int k = 0; k < N - 1; ++k) {
+        for (int i = startRow; i < endRow; ++i) {
+            double factor = A[i][k] / A[k][k];
+            for (int j = k; j < N; ++j) {
+                A[i][j] -= factor * A[k][j];
             }
+            b[i] -= factor * b[k];
+        }
+    }
+}
+
+void backwardSubstitution(vector<vector<double>>& A, vector<double>& b, vector<double>& x, int startRow, int endRow) {
+    for (int k = N - 1; k >= 0; --k) {
+        for (int i = startRow; i < endRow; ++i) {
+            x[i] = b[i];
+            for (int j = k + 1; j < N; ++j) {
+                x[i] -= A[i][j] * x[j];
+            }
+            x[i] /= A[i][k];
         }
     }
 }
@@ -27,50 +41,44 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-    // Рассчитываем количество строк, которые будет обрабатывать каждый процесс
     int rowsPerProcess = N / numProcesses;
     int startRow = rank * rowsPerProcess;
-    int endRow = (rank == numProcesses - 1) ? N : startRow + rowsPerProcess;
+    int endRow = (rank + 1) * rowsPerProcess;
+    if (rank == numProcesses - 1) {
+        endRow = N; 
+    }
 
-    // Инициализация матриц только на процессе с рангом 0
     vector<vector<double>> A(N, vector<double>(N));
-    vector<vector<double>> B(N, vector<double>(N));
-    vector<vector<double>> localA(endRow - startRow, vector<double>(N));
-    vector<vector<double>> C(endRow - startRow, vector<double>(N));
+    vector<double> b(N);
 
-    if (rank == 0) {
-        // Заполняем матрицы случайными числами от 0 до 9
-        srand(time(NULL));
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                A[i][j] = rand() % 10;
-                B[i][j] = rand() % 10;
-            }
+    vector<double> x(N);
+
+    srand(time(NULL));
+    for (int i = 0; i < N; ++i) {
+        b[i] = rand() % 10; 
+        for (int j = 0; j < N; ++j) {
+            A[i][j] = rand() % 10; 
         }
     }
 
-    // Измеряем время для умножения матриц с использованием MPI
     MPI_Barrier(MPI_COMM_WORLD);
     auto startTimeMPI = high_resolution_clock::now();
 
-    // Рассылаем части матрицы A всем процессам
-    MPI_Bcast(&A[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    forwardElimination(A, b, startRow, endRow);
 
-    // Распределяем часть матрицы A между процессами
-    MPI_Scatter(&A[startRow][0], (endRow - startRow) * N, MPI_DOUBLE, &localA[0][0], (endRow - startRow) * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    // Умножение части матрицы A на матрицу B
-    matrixMultiply(localA, B, C, 0, endRow - startRow);
-
-    // Собираем результаты на процессе с рангом 0
-    vector<vector<double>> globalC(N, vector<double>(N));
-    MPI_Gather(&C[0][0], (endRow - startRow) * N, MPI_DOUBLE, &globalC[startRow][0], (endRow - startRow) * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    backwardSubstitution(A, b, x, startRow, endRow);
 
     auto endTimeMPI = high_resolution_clock::now();
     auto mpiTime = duration_cast<milliseconds>(endTimeMPI - startTimeMPI);
 
-    // Вывод времени
+    vector<double> globalX(N);
+    MPI_Gather(&x[0], rowsPerProcess, MPI_DOUBLE, &globalX[0], rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
     if (rank == 0) {
+        cout << "Solution:" << endl;
+        for (int i = 0; i < N; ++i) {
+            cout << "x[" << i << "] = " << globalX[i] << endl;
+        }
         cout << "Time: " << mpiTime.count() / 1000.0 << " seconds" << endl;
     }
 
