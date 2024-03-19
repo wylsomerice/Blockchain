5698830b3251bb246bbc96c666e3b876742e3bdb
Artem Ponomarev <ponomarev@fort-telecom.ru>
2024-03-19 17:57:07 +0500

    3, 4

---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <mpi.h>
+
+using namespace std;
+using namespace std::chrono;
+
+const int N = 100; // Размерность матрицы
+
+// Умножение блока матрицы A на блок матрицы B
+void blockMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B, vector<vector<double>>& C, int startRowA, int endRowA) {
+    for (int i = startRowA; i < endRowA; ++i) {
+        for (int j = 0; j < N; ++j) {
+            C[i - startRowA][j] = 0;
+            for (int k = 0; k < N; ++k) {
+                C[i - startRowA][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, numProcesses;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+
+    // Вычисляем количество блоков
+    int numBlocks = N / numProcesses;
+
+    // Инициализация матриц только на процессе с рангом 0
+    vector<vector<double>> A(N, vector<double>(N));
+    vector<vector<double>> B(N, vector<double>(N));
+    vector<vector<double>> C(numBlocks, vector<double>(N)); // Результат для каждого процесса
+
+    if (rank == 0) {
+        // Заполняем матрицы случайными числами от 0 до 9
+        srand(time(NULL));
+        for (int i = 0; i < numBlocks; ++i) { // Изменили i < N на i < numBlocks
+            for (int j = 0; j < N; ++j) { // Оставили без изменений
+                C[i][j] = 0; // Добавили инициализацию матрицы C
+            }
+        }
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                A[i][j] = rand() % 10;
+                B[i][j] = rand() % 10;
+            }
+        }
+    }
+
+    // Измеряем время для умножения матриц с использованием MPI
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto startTimeMPI = high_resolution_clock::now();
+
+    // Рассылаем матрицу B всем процессам
+    MPI_Bcast(&B[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Вычисляем блоки и умножаем их
+    int startRowA = rank * numBlocks;
+    int endRowA = (rank == numProcesses - 1) ? N : startRowA + numBlocks;
+    blockMultiply(A, B, C, startRowA, endRowA);
+
+    // Собираем результаты на процессе с рангом 0
+    vector<vector<double>> globalC(N, vector<double>(N));
+    MPI_Gather(&C[0][0], numBlocks * N, MPI_DOUBLE, &globalC[startRowA][0], numBlocks * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    auto endTimeMPI = high_resolution_clock::now();
+    auto mpiTime = duration_cast<milliseconds>(endTimeMPI - startTimeMPI);
+
+    // Вывод времени
+    if (rank == 0) {
+        cout << "Time: " << mpiTime.count() / 1000.0 << " seconds" << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ Lab5/Lab5/Lab5.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <mpi.h>
+
+using namespace std;
+using namespace std::chrono;
+
+const int N = 100; // Размерность матрицы
+
+// Умножение части матрицы A на матрицу B
+void matrixMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B, vector<vector<double>>& C, int startRow, int endRow) {
+    for (int i = startRow; i < endRow; ++i) {
+        for (int j = 0; j < N; ++j) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, numProcesses;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+
+    // Рассчитываем количество строк, которые будет обрабатывать каждый процесс
+    int rowsPerProcess = N / numProcesses;
+    int startRow = rank * rowsPerProcess;
+    int endRow = (rank == numProcesses - 1) ? N : startRow + rowsPerProcess;
+
+    // Инициализация матриц только на процессе с рангом 0
+    vector<vector<double>> A(N, vector<double>(N));
+    vector<vector<double>> B(N, vector<double>(N));
+    vector<vector<double>> localA(endRow - startRow, vector<double>(N));
+    vector<vector<double>> C(endRow - startRow, vector<double>(N));
+
+    if (rank == 0) {
+        // Заполняем матрицы случайными числами от 0 до 9
+        srand(time(NULL));
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                A[i][j] = rand() % 10;
+                B[i][j] = rand() % 10;
+            }
+        }
+    }
+
+    // Измеряем время для умножения матриц с использованием MPI
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto startTimeMPI = high_resolution_clock::now();
+
+    // Рассылаем части матрицы A всем процессам
+    MPI_Bcast(&A[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Распределяем часть матрицы A между процессами
+    MPI_Scatter(&A[startRow][0], (endRow - startRow) * N, MPI_DOUBLE, &localA[0][0], (endRow - startRow) * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Умножение части матрицы A на матрицу B
+    matrixMultiply(localA, B, C, 0, endRow - startRow);
+
+    // Собираем результаты на процессе с рангом 0
+    vector<vector<double>> globalC(N, vector<double>(N));
+    MPI_Gather(&C[0][0], (endRow - startRow) * N, MPI_DOUBLE, &globalC[startRow][0], (endRow - startRow) * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    auto endTimeMPI = high_resolution_clock::now();
+    auto mpiTime = duration_cast<milliseconds>(endTimeMPI - startTimeMPI);
+
+    // Вывод времени
+    if (rank == 0) {
+        cout << "Time: " << mpiTime.count() / 1000.0 << " seconds" << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
