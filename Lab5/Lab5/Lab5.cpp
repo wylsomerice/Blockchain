303790ea641c54df3cf1e93a72d0e502ed6ac6e7
Artem Ponomarev(home) <baddassdude@mail.ru>
2024-03-31 21:43:42 +0500

    Исправил 5

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -2,84 +2,91 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <chrono>
 #include <mpi.h>
+#include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
-const int N = 2000; 
+// Генерация случайных чисел от 0 до 9
+int generateRandom() {
+    return rand() % 10;
+}
 
-void forwardElimination(vector<vector<double>>& A, vector<double>& b, int startRow, int endRow) {
-    for (int k = 0; k < N - 1; ++k) {
-        for (int i = startRow; i < endRow; ++i) {
-            double factor = A[i][k] / A[k][k];
-            for (int j = k; j < N; ++j) {
-                A[i][j] -= factor * A[k][j];
+// Метод Гаусса для решения СЛАУ на части матрицы и вектора
+void gaussElimination(vector<vector<double>>& A, vector<double>& b, int startRow, int endRow) {
+    const int n = A.size();
+
+    for (int i = 0; i < n; ++i) {
+        double pivot = A[i][i];
+        if (pivot == 0) {
+            throw runtime_error("Matrix is singular");
+        }
+
+        for (int j = i + 1; j < n; ++j) {
+            double ratio = A[j][i] / pivot;
+            for (int k = i; k < n; ++k) {
+                A[j][k] -= ratio * A[i][k];
             }
-            b[i] -= factor * b[k];
+            b[j] -= ratio * b[i];
+        }
+    }
+
+    // Обратный ход метода Гаусса
+    for (int i = n - 1; i >= 0; --i) {
+        b[i] /= A[i][i];
+        A[i][i] = 1.0;
+
+        for (int j = i - 1; j >= 0; --j) {
+            b[j] -= A[j][i] * b[i];
+            A[j][i] = 0.0;
         }
     }
 }
 
-void backwardSubstitution(vector<vector<double>>& A, vector<double>& b, vector<double>& x, int startRow, int endRow) {
-    for (int k = N - 1; k >= 0; --k) {
-        for (int i = startRow; i < endRow; ++i) {
-            x[i] = b[i];
-            for (int j = k + 1; j < N; ++j) {
-                x[i] -= A[i][j] * x[j];
-            }
-            x[i] /= A[i][k];
-        }
-    }
-}
-
-int main(int argc, char* argv[]) {
-    MPI_Init(&argc, &argv);
-
-    int rank, numProcesses;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-
-    int rowsPerProcess = N / numProcesses;
-    int startRow = rank * rowsPerProcess;
-    int endRow = (rank + 1) * rowsPerProcess;
-    if (rank == numProcesses - 1) {
-        endRow = N; 
-    }
-
-    vector<vector<double>> A(N, vector<double>(N));
-    vector<double> b(N);
-
-    vector<double> x(N);
-
+int main(int argc, char** argv) {
     srand(time(NULL));
-    for (int i = 0; i < N; ++i) {
-        b[i] = rand() % 10; 
-        for (int j = 0; j < N; ++j) {
-            A[i][j] = rand() % 10; 
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    const int N = 100; 
+
+    const int rowsPerProcess = N / size;
+    const int remainingRows = N % size;
+
+    vector<vector<double>> localA(rowsPerProcess + (rank < remainingRows ? 1 : 0), vector<double>(N));
+    vector<double> localB(rowsPerProcess + (rank < remainingRows ? 1 : 0));
+
+    if (rank == 0) {
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                localA[i][j] = generateRandom();
+            }
+            localB[i] = generateRandom();
         }
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    auto startTimeMPI = high_resolution_clock::now();
+    MPI_Scatter(localA[0].data(), rowsPerProcess * N, MPI_DOUBLE, localA[0].data(), rowsPerProcess * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(localB.data(), rowsPerProcess, MPI_DOUBLE, localB.data(), rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    forwardElimination(A, b, startRow, endRow);
+    auto start = high_resolution_clock::now();
+    gaussElimination(localA, localB, 0, localA.size());
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start).count();
 
-    backwardSubstitution(A, b, x, startRow, endRow);
-
-    auto endTimeMPI = high_resolution_clock::now();
-    auto mpiTime = duration_cast<milliseconds>(endTimeMPI - startTimeMPI);
-
-    vector<double> globalX(N);
-    MPI_Gather(&x[0], rowsPerProcess, MPI_DOUBLE, &globalX[0], rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    vector<double> result(N);
+    MPI_Gather(localB.data(), rowsPerProcess, MPI_DOUBLE, result.data(), rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         cout << "Solution:" << endl;
         for (int i = 0; i < N; ++i) {
-            cout << "x[" << i << "] = " << globalX[i] << endl;
+            cout << result[i] << " ";
         }
-        cout << "Time: " << mpiTime.count() / 1000.0 << " seconds" << endl;
+        cout << endl;
+        cout << "Time taken: " << duration << " milliseconds" << endl;
     }
 
     MPI_Finalize();
