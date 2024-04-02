6ffad8d034230355a98a88df1e0a7aea94ae1ebb
Artem Ponomarev(home) <baddassdude@mail.ru>
2024-04-02 21:58:04 +0500

    исправил 4 5

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -6,75 +6,55 @@
 using namespace std;
 using namespace std::chrono;
 
-const int N = 100; // Размерность матрицы
-
-// Умножение блока матрицы A на блок матрицы B
-void blockMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B, vector<vector<double>>& C, int startRowA, int endRowA) {
-    for (int i = startRowA; i < endRowA; ++i) {
+const int N = 100; 
+int B[N][N], A[N][N];
+int C[N][N];
+void setMatrix(int matrix[N][N]) {
+    for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            C[i - startRowA][j] = 0;
-            for (int k = 0; k < N; ++k) {
-                C[i - startRowA][j] += A[i][k] * B[k][j];
-            }
+            matrix[i][j] = rand() % 10 - 1;
         }
     }
 }
 
 int main(int argc, char* argv[]) {
+    int i, j, k, rank, size, sum = 0;
+    int localA[N], localC[N];
     MPI_Init(&argc, &argv);
-
-    int rank, numProcesses;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-    // Вычисляем количество блоков
-    int numBlocks = N / numProcesses;
-
-    // Инициализация матриц только на процессе с рангом 0
-    vector<vector<double>> A(N, vector<double>(N));
-    vector<vector<double>> B(N, vector<double>(N));
-    vector<vector<double>> C(numBlocks, vector<double>(N)); // Результат для каждого процесса
-
-    if (rank == 0) {
-        // Заполняем матрицы случайными числами от 0 до 9
-        srand(time(NULL));
-        for (int i = 0; i < numBlocks; ++i) { // Изменили i < N на i < numBlocks
-            for (int j = 0; j < N; ++j) { // Оставили без изменений
-                C[i][j] = 0; // Добавили инициализацию матрицы C
-            }
-        }
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                A[i][j] = rand() % 10;
-                B[i][j] = rand() % 10;
-            }
-        }
+    if (rank == 0)
+    {
+        setMatrix(A);     
     }
 
-    // Измеряем время для умножения матриц с использованием MPI
+    MPI_Scatter(A, N * N / size, MPI_INT, localA, N * N / size, MPI_INT, 0, MPI_COMM_WORLD);//Распределяем каждому процессу по одной строке матрицы
+    MPI_Bcast(B, N * N, MPI_INT, 0, MPI_COMM_WORLD);//Распределяем каждому процессу по одной строке матрицы
+
+    auto start = high_resolution_clock::now();
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            sum = sum + localA[j] * B[j][i];
+        }
+        localC[i] = sum;
+        sum = 0;
+    }
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start).count();
+
     MPI_Barrier(MPI_COMM_WORLD);
-    auto startTimeMPI = high_resolution_clock::now();
 
-    // Рассылаем матрицу B всем процессам
-    MPI_Bcast(&B[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gather(localC, N * N / size, MPI_INT, C, N * N / size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Вычисляем блоки и умножаем их
-    int startRowA = rank * numBlocks;
-    int endRowA = (rank == numProcesses - 1) ? N : startRowA + numBlocks;
-    blockMultiply(A, B, C, startRowA, endRowA);
-
-    // Собираем результаты на процессе с рангом 0
-    vector<vector<double>> globalC(N, vector<double>(N));
-    MPI_Gather(&C[0][0], numBlocks * N, MPI_DOUBLE, &globalC[startRowA][0], numBlocks * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    auto endTimeMPI = high_resolution_clock::now();
-    auto mpiTime = duration_cast<milliseconds>(endTimeMPI - startTimeMPI);
-
-    // Вывод времени
-    if (rank == 0) {
-        cout << "Time: " << mpiTime.count() / 1000.0 << " seconds" << endl;
+    if (rank == 0)//Вывод финальной матрицы после синхронизации и сбора данных 
+    {
+        cout << endl << endl << "Time with MPI: " << duration << endl << endl;
     }
 
     MPI_Finalize();
-    return 0;
 }
--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -8,87 +8,151 @@
 using namespace std;
 using namespace std::chrono;
 
-// Генерация случайных чисел от 0 до 9
+const int N = 200; 
+double A[N][N + 1], B[N][N + 1];
+
 int generateRandom() {
     return rand() % 10;
 }
 
-// Метод Гаусса для решения СЛАУ на части матрицы и вектора
-void gaussElimination(vector<vector<double>>& A, vector<double>& b, int startRow, int endRow) {
-    const int n = A.size();
+void gaussElimination(double A[N][N + 1]) {
+	auto start = high_resolution_clock::now();
+	
+	// Прямой ход
+	for (int k = 0; k < N - 1; k++) {
 
-    for (int i = 0; i < n; ++i) {
-        double pivot = A[i][i];
-        if (pivot == 0) {
-            throw runtime_error("Matrix is singular");
-        }
+		for (int i = k + 1; i < N; i++) {
+			double koef = A[i][k] / A[k][k];
+			for (int j = k; j < N + 1; j++) {
+				A[i][j] -= koef * A[k][j];
+			}
+		}
+	}
+	// Обратный ход
+	for (int k = N - 1; k >= 0; k--) {
+		A[k][N] /= A[k][k];
+		A[k][k] = 1.0;
+		for (int i = 0; i < k; i++) {
+			A[i][N] -= A[i][k] * A[k][N];
+			A[i][k] = 0.0;
+		}
+	}
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(end - start).count();
 
-        for (int j = i + 1; j < n; ++j) {
-            double ratio = A[j][i] / pivot;
-            for (int k = i; k < n; ++k) {
-                A[j][k] -= ratio * A[i][k];
-            }
-            b[j] -= ratio * b[i];
-        }
-    }
-
-    // Обратный ход метода Гаусса
-    for (int i = n - 1; i >= 0; --i) {
-        b[i] /= A[i][i];
-        A[i][i] = 1.0;
-
-        for (int j = i - 1; j >= 0; --j) {
-            b[j] -= A[j][i] * b[i];
-            A[j][i] = 0.0;
-        }
-    }
+	cout << endl << endl << "Time without MPI: " << duration;
 }
 
+
 int main(int argc, char** argv) {
-    srand(time(NULL));
-    int rank, size;
+	int rank;
+	int size;
+	int block;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Init(&argc, &argv);
 
-    const int N = 100; 
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int rowsPerProcess = N / size;
-    const int remainingRows = N % size;
+	block = N / size;
 
-    vector<vector<double>> localA(rowsPerProcess + (rank < remainingRows ? 1 : 0), vector<double>(N));
-    vector<double> localB(rowsPerProcess + (rank < remainingRows ? 1 : 0));
+	if (rank == 0)
+	{
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N + 1; ++j) {
+				A[i][j] = generateRandom();
+				B[i][j] = generateRandom();
+			}
+		}
+	}
 
-    if (rank == 0) {
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                localA[i][j] = generateRandom();
-            }
-            localB[i] = generateRandom();
-        }
-    }
+	auto timerStart = high_resolution_clock::now();
 
-    MPI_Scatter(localA[0].data(), rowsPerProcess * N, MPI_DOUBLE, localA[0].data(), rowsPerProcess * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatter(localB.data(), rowsPerProcess, MPI_DOUBLE, localB.data(), rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    auto start = high_resolution_clock::now();
-    gaussElimination(localA, localB, 0, localA.size());
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start).count();
+	MPI_Bcast(&A[0][0], N * (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    vector<double> result(N);
-    MPI_Gather(localB.data(), rowsPerProcess, MPI_DOUBLE, result.data(), rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// Прямой ход
+	int start = rank * block;
+	int end = start + block;
+	double temp[N][N + 1];
 
-    if (rank == 0) {
-        cout << "Solution:" << endl;
-        for (int i = 0; i < N; ++i) {
-            cout << result[i] << " ";
-        }
-        cout << endl;
-        cout << "Time taken: " << duration << " milliseconds" << endl;
-    }
+	for (int k = 0; k < N - 1; k++) {
 
-    MPI_Finalize();
-    return 0;
+		if (end - 1 > k) {
+			for (int i = start; i < end; i++) {
+				if (i > k) {
+					double koef = A[i][k] / A[k][k];
+					for (int j = k; j < N + 1; j++) {
+						A[i][j] -= koef * A[k][j];
+					}
+				}
+
+			}
+		}
+
+		MPI_Gather(&A[start][0], block * (N + 1), MPI_DOUBLE, &temp[start][0], block * (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+		if (rank == 0)
+		{
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < N + 1; j++)
+				{
+					A[i][j] = temp[i][j];
+				}
+			}
+		}
+		MPI_Bcast(&A[0][0], N * (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	}
+
+
+	// Обратный ход
+	for (int k = N - 1; k >= 0; k--) {
+		if (rank == 0)
+		{
+			A[k][N] /= A[k][k];
+			A[k][k] = 1.0;
+		}
+		MPI_Bcast(&A[k][0], (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+		for (int i = start; i < end; i++) {
+			if (i < k)
+			{
+				A[i][N] -= A[i][k] * A[k][N];
+				A[i][k] = 0.0;
+			}
+		}
+
+		MPI_Gather(&A[start][0], block * (N + 1), MPI_DOUBLE, &temp[start][0], block * (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+		if (rank == 0)
+		{
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < N + 1; j++)
+				{
+					A[i][j] = temp[i][j];
+				}
+			}
+		}
+
+		MPI_Bcast(&A[0][0], N * (N + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+		
+
+		
+	}
+
+	auto timerEnd = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(timerEnd - timerStart).count();
+
+	if (rank == 0)
+	{
+		cout << endl << endl << "Time with MPI: " << duration << endl << endl;
+
+		gaussElimination(B);
+	}
+
+	MPI_Finalize();
+	return 0;
 }
